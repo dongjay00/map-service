fa3c11326eea29e167f3548f01e5ad190c2fd431
dongjay00 <cjswodlehdgn@gmail.com>
2023-03-10 22:07:14 +0900

    feat: add Header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,11 @@
+import { Fragment } from 'react';
+import Header from '../components/common/Header';
+
 export default function Home() {
-  return <>Hello World!</>;
+  return (
+    <Fragment>
+      <Header />
+      <main></main>
+    </Fragment>
+  );
 }
